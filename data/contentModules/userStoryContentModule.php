0f8869b605bd9ff1f21e93ea71cda2a39dfc2895
David Hondl <david.hondl@gmx.net>
2015-12-03 18:40:07 +0100

    userStory is here
    information and links for every story is displayed now

--- data/contentModules/userStoryContentModule.php
+++ data/contentModules/userStoryContentModule.php
@@ -18,16 +18,42 @@ class userStoryContentModule{
 
 	function generateHtml(){
 	
-		$returnString = '';
+		$msqlObject = new mysqlModule();
 	
-		if($this->sessionObject->getLogState()){
-			$returnString.='Hey '.$this->sessionObject->getUserName().'!<br/>'."\n";
+		$returnString = '';
+		
+		$template = new contentTemplateModule('userStoryTemplate');	
+		
+		$queryResult = array();
+		$userQueryResult = array();
+		$user2QueryResult = array();
+		
+		$userIDQueryResult = $msqlObject->queryDataBase('SELECT id FROM users WHERE name = "'.$this->searchedUser.'"');
+		if(isset($userIDQueryResult[0]['id'])){
+			$userID = $userIDQueryResult[0]['id'];
+			$queryResult = $msqlObject->queryDataBase('SELECT * FROM story WHERE user = "'.$userID.'" AND name = "'.$this->searchedStory.'"');
+
+			if(isset($queryResult[0]['name'])){
+			$returnString.='<h2>'.$this->searchedStory.'</h2>'."\n";
+			$returnString.='<p><a href="published">look at it</a></p>'."\n";
+			if($this->searchedUser === $this->sessionObject->getUserName()){
+				$returnString.='<p><a href="edit">edit</a></p>'."\n";
+			}
+		}else{
+			$returnString.='<h2>no story found!</h2>'."\n";
+		}
+			
+		}else{
+			$returnString.='<h2>no story found!</h2>'."\n";
 		}
 		
-		$returnString.='Hier werden Infos zu folgender Story ausgegeben: '.$this->searchedStory.',<br/>';
-		$returnString.='von folgendem User: '.$this->searchedUser;
 		
-		return $returnString;
+		
+		$template->addLogState($this->sessionObject);
+		$template->addContent('INFO', $returnString);
+
+	
+		return $template->generateHtml();
 		
 	}
 	
