d7f412e12c59ade0a9f37e9947968b5f1f8a36ac
David Hondl <david.hondl@gmx.net>
2015-12-04 13:04:51 +0100

    Bugfixes
    WHATEVER FKING BUGS FKING GIT

--- data/contentModules/indexContentModule.php
+++ data/contentModules/indexContentModule.php
@@ -29,7 +29,7 @@ class indexContentModule{
 			//	</form>'."\n";
 			return '<form action="search" method="get">'."\n".'
 				<p class="searchLabel">SEARCH:
-					<input class="inputField" type="text" name="key" value="'.$key.'"/>
+					<input class="inputField" type="text" name="key"/>
 					<input class="button searchIcon" type="submit" value="&#xf002;"/>
 				</p>'."\n".'
 				</form>'."\n";
