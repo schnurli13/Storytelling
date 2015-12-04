8fe5afa30a3d1469a155f73a0f8d28cf71882ef4
Eva Loidl <eva.loidl@gmx.at>
2015-12-04 14:47:43 +0100

    Index/SearchNew

--- data/contentModules/indexContentModule.php
+++ data/contentModules/indexContentModule.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /**
  * Created by David Hondl.
@@ -25,52 +24,10 @@ class indexContentModule{
 	
 		function getForm(){
 		return '<form action="search" method="get">'."\n".'
-			<p class="searchLabel">SEARCH:
-				<input class="inputField" type="text" name="key"/>'."\n".'
-				<input class="button searchIcon" type="submit" value="&#xf002;"/>'."\n".'
-			</p>
+			<p><input type="text" name="key"/></p>'."\n".'
+			<p><input type="submit" value="search"/></p>'."\n".'
 			</form>'."\n";
 	}
 
 	
-=======
-<?php
-/**
- * Created by David Hondl.
- * Version: 0.1
- */
-
-class indexContentModule{
-
-	private $sessionObject;
-	
-	function __construct($sessionObject){
-		$this->sessionObject = $sessionObject;
-	}
-	
-	function generateHtml(){
-	
-		$template = new contentTemplateModule('indexTemplate');
-		$template->addLogState($this->sessionObject);
-		$template->addContent('FORM',$this->getForm());
-		
-		return $template->generateHtml();
-		
-	}
-	
-		function getForm(){
-			//return '<form action="search" method="get">'."\n".'
-			//	<p class="searchLabel">SEARCH:<input class="inputField" type="text" name="key" value="'.$key.'"/></p>'."\n".'
-			//	<div class="buttonFrameContainer"><input class="button" type="submit" value="SEARCH"/></div>'."\n".'
-			//	</form>'."\n";
-			return '<form action="search" method="get">'."\n".'
-				<p class="searchLabel">SEARCH:
-					<input class="inputField" type="text" name="key"/>
-					<input class="button searchIcon" type="submit" value="&#xf002;"/>
-				</p>'."\n".'
-				</form>'."\n";
-	}
-
-	
->>>>>>> origin/master
 }
--- data/contentModules/searchContentModule.php
+++ data/contentModules/searchContentModule.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /**
  * Created by David Hondl.
@@ -58,83 +57,9 @@ class searchContentModule{
 	
 		function getForm($key){
 			return '<form action="search" method="get">'."\n".'
-				<p class="searchLabel">SEARCH:
-					<input class="inputField" type="text" name="key" value="'.$key.'"/>'."\n".'
-					<input class="button searchIcon" type="submit" value="&#xf002;"/>'."\n".'
-				</p>
+				<p><input type="text" name="key" value="'.$key.'"/></p>'."\n".'
+				<p><input type="submit" value="search"/></p>'."\n".'
 				</form>'."\n";
 		}
 	
-=======
-<?php
-/**
- * Created by David Hondl.
- * Version: 0.1
- */
-
-class searchContentModule{
-
-	private $sessionObject;
-	
-	private $urlArray = array();
-
-	function __construct($sessionObject, $urlArray){
-		$this->sessionObject = $sessionObject;
-		$this->urlArray = $urlArray;
-	}
-
-	function generateHtml(){
-	
-		$msqlObject = new mysqlModule();
-	
-		$returnString = '';
-		
-		$template = new contentTemplateModule('searchTemplate');	
-		
-		if(isset($_GET['key'])){
-			$returnString.=$this->getForm($_GET['key']);
-		}else{
-			$returnString.=$this->getForm('');
-		}
-		
-		$queryResult = array();
-		
-		if(isset($_GET['key'])){
-			$queryResult = $msqlObject->queryDataBase('SELECT name, password FROM users WHERE name LIKE "%'.$_GET['key'].'%"');
-		}
-		
-		if(isset($queryResult[0]['name'])){
-		
-			$returnString.='<h2>Found Users:</h2>';
-			
-			$returnString.='<ul>';
-			
-			for($i = 0; $i < sizeof($queryResult); $i++){
-				$returnString.='<li><a href="'.$this->urlArray[0].'/users/'.$queryResult[$i]['name'].'">'.$queryResult[$i]['name'].'</a></li>';
-			}
-			
-			$returnString.='</ul>';
-		
-		}
-	
-		$template->addLogState($this->sessionObject);
-		$template->addContent('FORM', $returnString);
-		
-		 return $template->generateHtml();
-	}
-	
-		function getForm($key){
-			//return '<form action="search" method="get">'."\n".'
-			//	<p class="searchLabel">SEARCH:<input class="inputField" type="text" name="key" value="'.$key.'"/></p>'."\n".'
-			//	<div class="iconFrameContainer"><input class="button searchIcon" type="submit" value="&#xf002;"/></div>'."\n".'
-			//	</form>'."\n";
-			return '<form action="search" method="get">'."\n".'
-				<p class="darkBlue searchLabel">SEARCH:
-					<input class="inputField" type="text" name="key" value="'.$key.'"/>
-					<input class="button searchIcon" type="submit" value="&#xf002;"/>
-				</p>'."\n".'
-				</form>'."\n";
-		}
-	
->>>>>>> origin/master
 }
