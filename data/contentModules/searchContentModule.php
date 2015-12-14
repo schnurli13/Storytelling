4891a94c5154b257754aecab4e2591792c8dc5d5
David Hondl <david.hondl@gmx.net>
2015-12-14 16:54:48 +0100

    Update for search
    Search now queries for stories and shows the fitting images.

--- data/contentModules/searchContentModule.php
+++ data/contentModules/searchContentModule.php
@@ -7,17 +7,17 @@
 class searchContentModule{
 
 	private $sessionObject;
-	
+	private $msqlObject;
 	private $urlArray = array();
 
 	function __construct($sessionObject, $urlArray){
 		$this->sessionObject = $sessionObject;
 		$this->urlArray = $urlArray;
+		$this->msqlObject = new mysqlModule();
 	}
 
 	function generateHtml(){
 	
-		$msqlObject = new mysqlModule();
 	
 		$returnString = '';
 		
@@ -29,26 +29,9 @@ class searchContentModule{
 			$returnString.=$this->getForm('');
 		}
 		
-		$queryResult = array();
+		$returnString.=$this->fetchUsers();
+		$returnString.=$this->fetchStories();
 		
-		if(isset($_GET['key']) && $_GET['key'] != ''){
-			$queryResult = $msqlObject->queryDataBase('SELECT name, password FROM users WHERE name LIKE "%'.$_GET['key'].'%"');
-		}
-		
-		if(isset($queryResult[0]['name'])){
-		
-			$returnString.='<h2>USER</h2>';
-			
-			$returnString.='<ul>';
-			
-			for($i = 0; $i < sizeof($queryResult); $i++){
-				$returnString.='<li><img class="searchPic" src="'.$this->urlArray[0].'/public/images/dummyProfile.jpg" alt="profil" /><a class="searchLink" href="'.$this->urlArray[0].'/users/'.$queryResult[$i]['name'].'">'.$queryResult[$i]['name'].'</a></li>';
-			}
-			
-			$returnString.='</ul>';
-		
-		}
-	
 		$template->addLogState($this->sessionObject);
 		$template->addContent('FORM', $returnString);
 		
@@ -63,5 +46,47 @@ class searchContentModule{
 				</p>
 				</form>'."\n";
 		}
-	
+		
+		function fetchUsers(){
+			$returnString = '';
+			$queryResult = array();	
+			if(isset($_GET['key']) && $_GET['key'] != ''){
+				$queryResult = $this->msqlObject->queryDataBase('SELECT * FROM users WHERE name LIKE "%'.$_GET['key'].'%"');
+			}
+			if(isset($queryResult[0]['name'])){
+				$returnString.='<div class="foundUsers">'."\n";
+				$returnString.='<h2>USERS</h2>'."\n";
+				$returnString.='<ul>'."\n";
+				for($i = 0; $i < sizeof($queryResult); $i++){
+					$storyImagePath = ($queryResult[$i]['imgPath'] != '') ? $this->urlArray[0].'/public/images/profile/'.$queryResult[$i]['imgPath'] : $this->urlArray[0].'/public/images/dummyProfile.jpg';
+					$returnString.='<li><img class="searchPic" src="'.$storyImagePath.'" alt="profil" /><a class="searchLink" href="'.$this->urlArray[0].'/users/'.$queryResult[$i]['name'].'">'.$queryResult[$i]['name'].'</a></li>'."\n";
+				}
+				$returnString.='</ul>'."\n";
+				$returnString.='</div>'."\n";
+			}
+			return $returnString;
+		}
+		
+		function fetchStories(){
+			$returnString = '';
+			$queryResult = array();	
+			$queryUserResult = array();
+			if(isset($_GET['key']) && $_GET['key'] != ''){
+				$queryResult = $this->msqlObject->queryDataBase('SELECT * FROM story WHERE name LIKE "%'.$_GET['key'].'%"');
+				$queryUserResult = $this->msqlObject->queryDataBase('SELECT name FROM story WHERE name LIKE "%'.$_GET['key'].'%"');
+			}
+			if(isset($queryResult[0]['name'])){
+				$returnString.='<div class="foundStories">'."\n";
+				$returnString.='<h2>STORIES</h2>'."\n";
+				$returnString.='<ul>'."\n";
+				for($i = 0; $i < sizeof($queryResult); $i++){
+					$fetchedUser = $this->msqlObject->queryDataBase('SELECT name FROM users WHERE id = "'.$queryResult[$i]['user'].'"');
+					$storyImagePath = ($queryResult[$i]['imgPath'] != '') ? $this->urlArray[0].'/public/images/story/'.$queryResult[$i]['imgPath'] : $this->urlArray[0].'/public/images/dummyStory.jpg';
+					$returnString.='<li><img class="searchPic" src="'.$storyImagePath.'" alt="profil" /><a class="searchLink" href="'.$this->urlArray[0].'/users/'.$fetchedUser[0]['name'].'/'.$queryResult[$i]['name'].'">'.$queryResult[$i]['name'].'</a></li>'."\n";
+				}
+				$returnString.='</ul>'."\n";
+				$returnString.='</div>'."\n";
+			}
+			return $returnString;
+		}
 }
