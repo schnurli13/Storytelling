f742663c62463dcefabed4af798e0bd18ae7fb74
Barbara <barbara-sikora@gmx.at>
2016-01-28 14:45:38 +0100

    NodeEditor changed reload

--- public/javascript/function.js
+++ public/javascript/function.js
@@ -2077,14 +2077,22 @@ nodeEditor.module = (function($) {
 
 
         //refresh page on browser resize
-        $(window).bind('resize', function(e)
+       /* $(window).bind('resize', function(e)
         {
             if (window.RT) clearTimeout(window.RT);
             window.RT = setTimeout(function()
             {
-                this.location.reload(false); /* false to get page from cache */
-            }, 500);
+                this.location.reload(false);
+           }, 500);
+        });*/
+
+        $(window).resize( function() {
+
+            window.location.href = window.location.href;
+
         });
+
+
 
 
         //fill in story details
